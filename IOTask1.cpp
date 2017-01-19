2ff24a9ae22e947cb186b9b220aadae934675cc7
Ilushkin <idenisov@ptsecurity.com>
2017-01-19 13:15:20 +0300

    first commit

---
+++ IOTask1.cpp
@@ -0,0 +1,13 @@
+// IOTask1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int main() {
+    int N = 0, K = 0;
+    std::cin >> N >> K;
+    std::cout << K % N;
+    return 0;
+
+
