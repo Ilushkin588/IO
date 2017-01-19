489a245d83b6c25ebc9597fe9bea3d0b815c032d
Ilushkin <denisow.ilya2009@yandex.ru>
2017-01-19 15:26:15 +0300

    added main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  IOTask3
+//
+//  Created by IlyaDenisov on 19.01.17.
+//  Copyright © 2017 Denisov's. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+
+    return 0;
+}
