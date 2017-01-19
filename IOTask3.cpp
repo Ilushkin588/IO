d3c62843c8b8477d2e0f518deb93dfe71bafd844
Ilushkin <denisow.ilya2009@yandex.ru>
2017-01-19 16:45:44 +0300

    Real Task3

--- IOTask3.cpp
+++ IOTask3.cpp
@@ -1,15 +1,8 @@
-//
-//  main.cpp
-//  IOTask3
-//
-//  Created by IlyaDenisov on 19.01.17.
-//  Copyright © 2017 Denisov's. All rights reserved.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-
-    return 0;
+int main() {
+    int N;
+	std::cin >> N;
+	std::cout << N / 10 % 10;
+  return 0;
 }
