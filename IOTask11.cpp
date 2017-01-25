e37c2eaa4a7804e60c5469c4f4ba34f119081304
Ilushkin <idenisov@ptsecurity.com>
2017-01-25 14:55:18 +0300

    With Function

--- IOTask11.cpp
+++ IOTask11.cpp
@@ -3,88 +3,52 @@
 #include "stdafx.h"
 #include <iostream>
 
+
+void rhombus(char symbol, int width, int height) {
+    if (width == 1 && height == 1) {
+        std::cout << " " << symbol << "\n" << symbol << " " << symbol << "\n" << " " << symbol << "\n";
+    }
+    else {
+        for (int i = 0; i <= width / 2; i++)
+            std::cout << " ";
+        std::cout << symbol;
+
+        for (int i = 0; i < height / 2; i++)
+            std::cout << "\n";
+        std::cout << symbol;
+
+        for (int i = 0; i <= width; i++)
+            std::cout << " ";
+        std::cout << symbol;
+
+        for (int i = 0; i < height / 2; i++)
+            std::cout << "\n";
+
+        for (int i = 0; i <= width / 2; i++)
+            std::cout << " ";
+        std::cout << symbol;
+    }
+}
 int main()
 {
     int symbol_num = 0, width = 0, height = 0;
+    char symbol;
     std::cout << "Enter the number of one of the elements(1: * , 2: # , 3: +): ";
     std::cin >> symbol_num >> width >> height;
     switch (symbol_num) {
     case 1: {
-        if (width == 1 && height == 1) {
-            std::cout << " *\n" << "* * \n" << " *\n";
-        }
-        else {
-            for (int i = 0; i <= width / 2; i++)
-                std::cout << " ";
-            std::cout << "*";
-
-            for (int i = 0; i < height / 2; i++)
-                std::cout << "\n";
-            std::cout << "*";
-
-            for (int i = 0; i <= width; i++)
-                std::cout << " ";
-            std::cout << "*";
-
-            for (int i = 0; i < height / 2; i++)
-                std::cout << "\n";
-
-            for (int i = 0; i <= width / 2; i++)
-                std::cout << " ";
-            std::cout << "*";
-        }
+        symbol = '*';
+        rhombus(symbol, width, height);
         break;
     }
     case 2: {
-        if (width == 1 && height == 1) {
-            std::cout << " #\n" << "# # \n" << " #\n";
-        }
-        else {
-            for (int i = 0; i <= width / 2; i++)
-                std::cout << " ";
-            std::cout << "#";
-
-            for (int i = 0; i < height / 2; i++)
-                std::cout << "\n";
-            std::cout << "#";
-
-            for (int i = 0; i <= width; i++)
-                std::cout << " ";
-            std::cout << "#";
-
-            for (int i = 0; i < height / 2; i++)
-                std::cout << "\n";
-
-            for (int i = 0; i <= width / 2; i++)
-                std::cout << " ";
-            std::cout << "#";
-        }
+        symbol = '#';
+        rhombus(symbol, width, height);
         break;
     }
     case 3: {
-        if (width == 1 && height == 1) {
-            std::cout << " +\n" << "+ + \n" << " +\n";
-        }
-        else {
-            for (int i = 0; i <= width / 2; i++)
-                std::cout << " ";
-            std::cout << "+";
-
-            for (int i = 0; i < height / 2; i++)
-                std::cout << "\n";
-            std::cout << "+";
-
-            for (int i = 0; i <= width; i++)
-                std::cout << " ";
-            std::cout << "+";
-
-            for (int i = 0; i < height / 2; i++)
-                std::cout << "\n";
-
-            for (int i = 0; i <= width / 2; i++)
-                std::cout << " ";
-            std::cout << "+";
-        }
+        symbol = '+';
+        rhombus(symbol, width, height);
         break;
     }
     }
