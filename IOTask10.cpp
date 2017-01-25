4c57f1e357f4d2c59ad2ddee332d08c52922ae9e
Ilushkin <idenisov@ptsecurity.com>
2017-01-25 12:53:03 +0300

    Changed if clauses

--- IOTask10.cpp
+++ IOTask10.cpp
@@ -11,12 +11,14 @@ int main()
     double A = 0.0, B = 0.0, C = 0.0;
     std::cin >> A >> B >> C;
     std::cout << std::setprecision(3) << std::fixed;
-    if (A + B > C)
+    auto Sum = A + B;
+    if (Sum - C > 0.00001) {
         std::cout << (A / C) + B;
-    else if (A + B < C)
-        std::cout << (A / B) + C;
+    }
+    else if (Sum - C < 0.00001)
+        std::cout << ((A / B) + C);
     else
-        std::cout << (A + B) / C;
+        std::cout << ((A + B) / C);
     return 0;
 }
 
