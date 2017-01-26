5f8ecbe9d03f59e7ab692849e1809328e4fd295e
Ilushkin <idenisov@ptsecurity.com>
2017-01-26 15:51:39 +0300

    Eleventh Programm Final

--- IOTask11.cpp
+++ IOTask11.cpp
@@ -2,34 +2,55 @@
 //
 #include "stdafx.h"
 #include <iostream>
-
 void print(char symbol, int amount) {
-    for (int i = 0; i < amount; i++) {
+    for (int i = 0; i < amount; ++i) {
         std::cout << symbol;
     }
 }
-int main() {
-    int H = 0, W = 0;
-    std::cin >> H >> W;
-    char space = ' ';
+char getSymbol(int symbol_num) {
     char symbol = '*';
-    auto amountSpaces = H / 2;
-    auto amountSymb = 1;
-    for (int i = 1; i <= H/2 + 1; i++) {
-            print(space, amountSpaces);
-            print(symbol, amountSymb);
-            amountSpaces--;
-            amountSymb += 2;
-            std::cout << "\n";
+    switch (symbol_num) {
+    case 2: {
+        symbol = '#';
+        break;
     }
-    amountSymb = H - 2;
-    amountSpaces = 1;
-    for (int i = 1; i <= H/2; i++) {
-            print(space, amountSpaces);
-            print(symbol, amountSymb);
-            amountSpaces++;
-            amountSymb -= 2;
-            std::cout << "\n";
+    case 3: {
+        symbol = '+';
+        break;
     }
+    default: {
+        symbol = '*';
+        break;
+    }
+    }
+    return symbol;
+}
+void rhombes(int H, char symbol) {
+    auto amountSpaces = H / 2 + 1;
+    auto amountSymb = -1;
+    for (int i = 1; i <= H / 2 + 1; ++i) {
+        --amountSpaces;
+        amountSymb += 2;
+        print(' ', amountSpaces);
+        print(symbol, amountSymb);
+        std::cout << "\n";
+    }
+    for (int i = 0; i < H / 2; ++i) {
+        ++amountSpaces;
+        amountSymb -= 2;
+        print(' ', amountSpaces);
+        print(symbol, amountSymb);
+        std::cout << "\n";
+    }
+}
+
+int main() {
+    int H = 0, W = 0, symbol_num = 0;
+    std::cin >> H >> W;
+    
+    std::cout << "Enter the number of one of the elements(1: * , 2: # , 3: +): ";
+    std::cin >> symbol_num;
+    auto symbol = getSymbol(symbol_num);
+    rhombes(H, symbol);
     return 0;
 }
