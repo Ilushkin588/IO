395092c515804e7c55f60a76a8d7660a4b704383
Ilushkin <idenisov@ptsecurity.com>
2017-01-31 15:17:04 +0300

    New

--- IOTask11Project/DrawRhombes.h
+++ IOTask11Project/DrawRhombes.h
@@ -5,9 +5,17 @@
 
 class Rhombes {
 //private:
-   int H, symbol_num;
-   int * pH = &H;
-   int * pS_N = &symbol_num;
+    struct parametrs { 
+        int Height;
+        int symbol_number;
+        
+       parametrs() : Height(0), symbol_number(0){
+           std::cout << "Constructor was called!!\n";
+       }
+    };
+    
+    parametrs ofRhombes;
+   //int Height, symbol_number;
    
    char GetSymb(int symbol_num) {
        char symbol = '*';
@@ -28,71 +36,53 @@ class Rhombes {
        return symbol;
    }
 
-   void print(char symbol, int amount) {
-       for (int i = 0; i < amount; ++i) {
-           std::cout << symbol;
-       }
-   }
+ void print(char symbol, int amount);
    
 public:
-    
-    Rhombes(int H, int symbol_num) {
-        setRhombes(H, symbol_num);
+    Rhombes() {
+        int H = 0, symbol_num = 0;
+        std::cin >> H;
+        std::cout << "Enter the number of one of the elements(1: * , 2: # , 3: +): ";
+        std::cin >> symbol_num;
+        ofRhombes.Height = H;
+        ofRhombes.symbol_number = symbol_num;
     }
-    void setRhombes(int H, int symbol_num) {
-        
-        *pH = H;
-        *pS_N = symbol_num;
-    }
+   // Rhombes(int H, int symbol_num) {
+   //     setRhombes(H, symbol_num);
+   // }
+ //   void setRhombes(int H, int symbol_num) {
+   //     std::cout << "Heights of the Rhombes is " << H << "\n";
+  //      std::cout << "And the symbol number is " << symbol_num << "\n";
+ //       ofRhombes.Height = H;
+ //       ofRhombes.symbol_number = symbol_num;
+ //   }
 
     
    void draw() {
-       auto symbol = GetSymb(*pS_N);
-
-       auto amountSpaces = H / 2 + 1;
+       auto symbol = GetSymb(ofRhombes.symbol_number);
+       auto amountSpaces = ofRhombes.Height / 2 + 1;
        auto amountSymb = -1;
-       for (int i = 1; i <= H / 2 + 1; ++i) {
+       for (int i = 1; i <= ofRhombes.Height / 2 + 1; ++i) {
            --amountSpaces;
            amountSymb += 2;
-           print(' ', amountSpaces);
-           print(symbol, amountSymb);
+          print(' ', amountSpaces);
+          print(symbol, amountSymb);
            std::cout << "\n";
        }
-       for (int i = 0; i < H / 2; ++i) {
+       for (int i = 0; i < ofRhombes.Height / 2; ++i) {
            ++amountSpaces;
            amountSymb -= 2;
            print(' ', amountSpaces);
            print(symbol, amountSymb);
            std::cout << "\n";
        }
+      // ofRhombes.SimpleConstructor(ofRhombes.Height);
     }
-
-
-
-
-    /*
-   
-    void drawRhombes(int H, int symbol_num) {
-
-       auto symbol = getSymbol(symbol_num);
-
-        auto amountSpaces = H / 2 + 1;
-        auto amountSymb = -1;
-        for (int i = 1; i <= H / 2 + 1; ++i) {
-            --amountSpaces;
-            amountSymb += 2;
-            print(' ', amountSpaces);
-            print(symbol, amountSymb);
-            std::cout << "\n";
-        }
-        for (int i = 0; i < H / 2; ++i) {
-            ++amountSpaces;
-            amountSymb -= 2;
-            print(' ', amountSpaces);
-            print(symbol, amountSymb);
-            std::cout << "\n";
-        }
-    }*/
+  
 };
-
+   void Rhombes::print (char symbol, int amount) {
+       for (int i = 0; i < amount; ++i) {
+           std::cout << symbol;
+       }
+   }
 #endif /* DrawRhombes_h */
--- IOTask11Project/proj11.cpp
+++ IOTask11Project/proj11.cpp
@@ -7,11 +7,8 @@
 
 
 int main() {
-    int H = 0, W = 0, symbol_num = 0;
-    std::cin >> H;
-    std::cout << "Enter the number of one of the elements(1: * , 2: # , 3: +): ";
-    std::cin >> symbol_num;
-    Rhombes Rhomb(H, symbol_num);
+    Rhombes Rhomb;
     Rhomb.draw();
+    
     return 0;
 }
