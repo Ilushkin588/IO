bfd62a7bb898d8376d53fac016edaab593328a95
Ilushkin <denisow.ilya2009@yandex.ru>
2017-02-09 02:08:11 +0300

    with calloc

--- DMTasks/DMTask1_2.cpp
+++ DMTasks/DMTask1_2.cpp
@@ -4,22 +4,22 @@
 int main()
 {
     int element = 1, size = 1, count = 0;
-    int *pArray =  (int *) malloc(size);
-        while (element > 0) {
-            std::cout << "Enter the number: ";
-            std::cin >> element;
-            if (element > 0) {
-                if (size <= count){
-                    pArray = (int *) realloc(pArray, ++size);
-                }
-                pArray[count] = element;
-                ++count;
+    int *pArray =  (int *) calloc(size, size * sizeof(int));// pArray = nullptr
+    while (element > 0) {
+        std::cout << "Enter the number: ";
+        std::cin >> element;
+        if (element > 0) {
+            if (size <= count){
+                pArray = (int *) realloc(pArray, ++size * sizeof(int));
             }
+            pArray[count] = element;
+            ++count;
         }
+    }
     
-        for (int i = 0; i < count; ++i) {
-            std::cout << pArray[i] << " ";
-        }
+    for (int i = 0; i < count; ++i) {
+        std::cout << pArray[i] << " ";
+    }
     free(pArray);
     return 0;
 }
