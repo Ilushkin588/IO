3e24dffdd68a270c9adcee3c5c994c61431ed0a8
Ilushkin <denisow.ilya2009@yandex.ru>
2017-02-08 17:21:08 +0300

    added realloc

--- DMTasks/DMTask1_2.cpp
+++ DMTasks/DMTask1_2.cpp
@@ -5,25 +5,21 @@ int main()
 {
     int element = 1, size = 1, count = 0;
     int *pArray =  (int *) malloc(size);
-    while (element > 0) {
-        std::cout << "Enter the number: ";
-        std::cin >> element;
-        if (element > 0) {
-            if (size <= count) {
-                int *temp = (int *) malloc(++size);
-                for (int i = 0; i < count; ++i) {
-                    temp[i] = pArray[i];
+        while (element > 0) {
+            std::cout << "Enter the number: ";
+            std::cin >> element;
+            if (element > 0) {
+                if (size <= count){
+                    pArray = (int *) realloc(pArray, ++size);
                 }
-                free (pArray);
-                pArray = temp;
+                pArray[count] = element;
+                ++count;
             }
-            pArray[count] = element;
-            ++count;
         }
-    }
-    for (int i = 0; i < count; ++i) {
-        std::cout << pArray[i] << " ";
-    }
+    
+        for (int i = 0; i < count; ++i) {
+            std::cout << pArray[i] << " ";
+        }
     free(pArray);
     return 0;
 }
